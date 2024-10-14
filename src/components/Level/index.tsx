09e59ed8aebb6f174ce29a97db90e290d602ef75
Pedro Dias <pdr050203@gmail.com>
2024-10-14 18:57:23 -0300

    feat: add animation based on scale on level component

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native';
+import { Pressable, PressableProps, Text } from 'react-native';
 
 import Animated, { 
   useSharedValue,
@@ -15,7 +15,7 @@ const TYPE_COLORS = {
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
 }
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
@@ -32,8 +32,16 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
     }
   })
 
+  function onPressIn() {
+    scale.value = 1.1;
+  }
+
+  function onPressOut() {
+    scale.value = 1;
+  }
+
   return (
-    <TouchableOpacity {...rest}>
+    <Pressable onPressIn={onPressIn} onPressOut={onPressOut} {...rest}>
       <Animated.View style={
         [
           styles.container,
@@ -49,6 +57,6 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           {title}
         </Text>
       </Animated.View>
-    </TouchableOpacity>
+    </Pressable>
   );
 }
