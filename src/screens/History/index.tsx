e80622fa36caf7c23af6ced69ec8b9b18d809dfb
Pedro Dias <pdr050203@gmail.com>
2024-10-16 21:44:47 -0300

    feat: add layout animations

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from '@react-navigation/native';
 import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { HouseLine } from 'phosphor-react-native';
 
+import Animated, { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated';
+
 import { Header } from '../../components/Header';
 import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
 
@@ -65,12 +67,18 @@ export function History() {
       >
         {
           history.map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => handleRemove(item.id)}
+            <Animated.View
+             key={item.id}
+             entering={SlideInRight}
+             exiting={SlideOutRight}
+             layout={Layout.springify()} 
             >
-              <HistoryCard data={item} />
-            </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => handleRemove(item.id)}
+              >
+                <HistoryCard data={item} />
+              </TouchableOpacity>
+            </Animated.View>
           ))
         }
       </ScrollView>
