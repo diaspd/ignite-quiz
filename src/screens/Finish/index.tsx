18eef4ee5dfd1f6b62686e68f745deef3e70458d
Pedro Dias <pdr050203@gmail.com>
2024-10-25 21:52:32 -0300

    add trophy and stars to finish screen

--- src/screens/Finish/index.tsx
+++ src/screens/Finish/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { Button } from '../../components/Button';
+import { Stars } from '../../components/Stars';
 
 import { styles } from './styles';
 
@@ -20,6 +21,9 @@ export function Finish() {
 
   return (
     <View style={styles.container}>
+
+      <Stars />
+
       <View style={styles.message}>
         <Text style={styles.title}>
           {isScoreGood ? 'Parabéns!' : 'Que pena'}
